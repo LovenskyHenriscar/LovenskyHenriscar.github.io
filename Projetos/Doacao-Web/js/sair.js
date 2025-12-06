080396a4359a1318c63312eb32353298fdb5b931
LovenskyHenriscar <henriscarlovensky02@gmail.com>
2025-12-06 16:51:54 -0300

    enviar

--- Projetos/Doacao-Web/js/sair.js
+++
@@ -1,9 +0,0 @@
-// js/sair.js
-function logout() {
-  // Limpa dados de sessão/localStorage
-  sessionStorage.clear();
-  localStorage.removeItem('authToken'); // ajuste conforme sua lógica
-
-  // Redireciona para a tela de login
-  window.location.href = 'login.html';
-}
